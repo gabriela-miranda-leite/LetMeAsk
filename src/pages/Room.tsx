82a6bf949da7f0a8298d0e4be01ba3cb3de99c5e
Gabriela <gabrielamiranda11.10@gmail.com>
2021-08-20 10:51:42 -0300

    adjust logic

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,94 +1,97 @@
-import logoImg from "../assets/images/logo.svg";
-import { Button } from "../components/Button";
-import { RoomCode } from "../components/RoomCode";
-import { useParams } from "react-router-dom";
-import "../styles/room.scss";
-import { useEffect, useState } from "react";
-import { useAuth } from "../hooks/useAuth";
-import { database } from "../services/firebase";
-import { FormEvent } from "react";
+import { useEffect } from 'react';
+import { FormEvent, useState } from 'react';
+import { useParams } from 'react-router-dom'
+
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 import { BiLoaderCircle } from "react-icons/bi";
 import toast, { Toaster } from "react-hot-toast";
+import '../styles/room.scss';
 
-type FirebaseQuestions = Record<
-  string,
-  {
-    author: {
-      name: string;
-      avatar: string;
-    };
-    content: string;
-    isAnswered: boolean;
-    isHighlighted: boolean;
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string;
+    avatar: string;
   }
->;
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+}>
 
 type Question = {
   id: string;
   author: {
     name: string;
     avatar: string;
-  };
+  }
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
-};
+}
 
 type RoomParams = {
   id: string;
-};
+}
 
-export const Room = () => {
+export function Room() {
+  const { user } = useAuth();
   const params = useParams<RoomParams>();
-  const roomID = params.id;
-  const [newQuestion, setNewQuestion] = useState("");
+  const [newQuestion, setNewQuestion] = useState('');
+  const [questions, setQuestions] = useState<Question[]>([])
+  const [title, setTitle] = useState('');
   const [load, setLoad] = useState(false);
-  const [title, setTitle] = useState("");
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const { signInWithGoogle, user } = useAuth();
+
+  const roomId = params.id;
 
   useEffect(() => {
-    const roomRef = database.ref(`rooms/${roomID}`);
-    roomRef.on("value", (room) => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on('value', room => {
       const databaseRoom = room.val();
-      setTitle(databaseRoom.title);
       const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-      const parsedQuestions = Object.entries(firebaseQuestions).map(
-        ([key, value]) => {
-          return {
-            id: key,
-            content: value.content,
-            author: value.author,
-            isHighlighted: value.isHighlighted,
-            isAnswered: value.isAnswered,
-          };
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighlighted: value.isHighlighted,
+          isAnswered: value.isAnswered,
         }
-      );
+      })
+
+      setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
-    });
-  }, [roomID]);
+    })
+  }, [roomId]);
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault();
-    if (newQuestion.trim() === "") {
+
+    if (newQuestion.trim() === '') {
       return;
     }
 
     if (!user) {
-      throw new Error("You must be logged in");
+        toast.error('Você precisa estar logado');
+        return;
     }
-
     const question = {
       content: newQuestion,
       author: {
         name: user.name,
         avatar: user.avatar,
       },
-      isHighLighted: false,
-      isAnswered: false,
+      isHighlighted: false,
+      isAnswered: false
     };
+
     setLoad(true);
-    await database.ref(`rooms/${roomID}/questions`).push(question);
+    await database.ref(`rooms/${roomId}/questions`).push(question);
     toast.success("Pergunta enviada!", {
       style: {
         borderRadius: "10px",
@@ -96,7 +99,9 @@ export const Room = () => {
         color: "#fff",
       },
     });
+
     setLoad(false);
+    setNewQuestion('');
   }
 
   return (
@@ -104,38 +109,32 @@ export const Room = () => {
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode code={roomID} />
+          <RoomCode code={roomId} />
         </div>
       </header>
 
       <main>
         <div className="room-title">
           <h1>Sala {title}</h1>
-          {questions.length === 1 ? (
-            <span>1 pergunta</span>
-          ) : (
-            <span>{questions.length} perguntas</span>
-          )}
+          { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
         </div>
 
         <form onSubmit={handleSendQuestion}>
           <textarea
-            onChange={(event) => setNewQuestion(event.target.value)}
-            value={newQuestion}
             placeholder="O que você quer perguntar?"
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
           />
+
           <div className="form-footer">
-            {user ? (
+            { user ? (
               <div className="user-info">
                 <img src={user.avatar} alt={user.name} />
                 <span>{user.name}</span>
               </div>
             ) : (
-              <span>
-                Para enviar uma pergunta,{" "}
-                <button onClick={signInWithGoogle}>faça seu login</button>.
-              </span>
-            )}
+              <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            ) }
             {load ? (
               <Button disabled>
                 <BiLoaderCircle />
@@ -148,8 +147,9 @@ export const Room = () => {
             )}
           </div>
         </form>
+
         {JSON.stringify(questions)}
       </main>
     </div>
   );
-};
+}
